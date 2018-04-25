cce887d5e0c8510cea1effa5b4bc8ac9c0bb20c2
ghorbanihamid <32678262+ghorbanihamid@users.noreply.github.com>
2018-04-25 16:01:02 +0430

    Add files via upload

--- src/main/db/javaexample.sql
+++ src/main/db/javaexample.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50524
  File Encoding         : 65001
 
- Date: 18/04/2018 19:17:49
+ Date: 25/04/2018 14:26:08
 */
 
 SET NAMES utf8mb4;
@@ -36,11 +36,6 @@ CREATE TABLE `customer`  (
 -- ----------------------------
 INSERT INTO `customer` VALUES (1000, 'hubert', 'hubert', 'hubert@yahoo.com', '09121822492', '2018-04-17 12:13:08');
 INSERT INTO `customer` VALUES (1001, 'john', 'john', 'john@yahoo.com', '09121822492', '2018-04-18 12:13:08');
-INSERT INTO `customer` VALUES (1002, 'rrrr', 'rrrr', 'e@yahoo.com', '11111111', NULL);
-INSERT INTO `customer` VALUES (1003, 'fff', 'fff', 'r@yahoo.com', '5555555', NULL);
-INSERT INTO `customer` VALUES (1004, 'www', 'www', 'w', 'w', NULL);
-INSERT INTO `customer` VALUES (1005, 'g', 'g', 'g', 'g', NULL);
-INSERT INTO `customer` VALUES (1006, 'ff', 'f', 'f', 'f', NULL);
 
 -- ----------------------------
 -- Table structure for customerseq
@@ -53,20 +48,53 @@ CREATE TABLE `customerseq`  (
 -- ----------------------------
 -- Records of customerseq
 -- ----------------------------
-INSERT INTO `customerseq` VALUES (1007);
+INSERT INTO `customerseq` VALUES (1018);
+
+-- ----------------------------
+-- Table structure for employee
+-- ----------------------------
+DROP TABLE IF EXISTS `employee`;
+CREATE TABLE `employee`  (
+  `EmployeeId` bigint(20) NOT NULL,
+  `FirstName` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `LastName` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `EmailAddress` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `PhoneNumber` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `CreateDate` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`EmployeeId`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of employee
+-- ----------------------------
+INSERT INTO `employee` VALUES (1000, 'hubert', 'hubert', 'hubert@yahoo.com', '09121822492', '2018-04-17 12:13:08');
+INSERT INTO `employee` VALUES (1004, 'Hamid', 'Ghorbani', 'ghorbani_1687@yahoo.com', '09121822492', '2018-04-24 16:58:26');
+
+-- ----------------------------
+-- Table structure for employeeseq
+-- ----------------------------
+DROP TABLE IF EXISTS `employeeseq`;
+CREATE TABLE `employeeseq`  (
+  `next_val` bigint(20) NULL DEFAULT NULL
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Fixed;
+
+-- ----------------------------
+-- Records of employeeseq
+-- ----------------------------
+INSERT INTO `employeeseq` VALUES (1005);
 
 -- ----------------------------
 -- Table structure for userinfo
 -- ----------------------------
 DROP TABLE IF EXISTS `userinfo`;
 CREATE TABLE `userinfo`  (
-  `UserId` int(11) NOT NULL,
+  `UserId` int(11) NOT NULL AUTO_INCREMENT,
   `UserFirstName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `UserLastName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `UserName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`UserId`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of userinfo
